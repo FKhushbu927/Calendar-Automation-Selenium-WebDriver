a45d7e05fc6dd4012abc759824a8e516f0c8c911
FKhushbu927 <jannateekhushbu927@gmail.com>
2025-02-14 22:56:40 +0600

    updated code

--- src/main/java/CalendarHandling.java
+++ src/main/java/CalendarHandling.java
@@ -16,14 +16,31 @@ public class CalendarHandling {
 
         driver.findElement(By.id("datepicker")).click();
 
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("ui-datepicker-calendar")));
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("ui-datepicker-calendar")));
 
 
        String monthYearVal =  driver.findElement(By.className("ui-datepicker-title")).getText();
 
-        System.out.println(monthYearVal);
+        System.out.println(monthYearVal);// February 2025
 
+        String month = monthYearVal.split(" ")[0].trim();
+        String year = monthYearVal.split(" ")[1].trim();
+
+        System.out.println(month);
+        System.out.println(year);
+
+        while (!(month.equals("June") && year.equals("2022"))) {
+            driver.findElement(By.xpath("//a[@title='Prev']")).click();
+            monthYearVal = driver.findElement(By.className("ui-datepicker-title")).getText();
+
+
+            System.out.println(monthYearVal);// February 2022
+
+            month = monthYearVal.split(" ")[0].trim();
+            year = monthYearVal.split(" ")[1].trim();
+        }
+
+        driver.findElement(By.xpath("//a[text() = '21']")).click();
 
     }
 }
