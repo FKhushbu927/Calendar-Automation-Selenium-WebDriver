9072543e219d9109e91a741a09f8835998b9ce6a
FKhushbu927 <jannateekhushbu927@gmail.com>
2025-02-14 18:16:05 +0600

    updated with calendar Tittle

--- src/main/java/CalendarHandling.java
+++ src/main/java/CalendarHandling.java
@@ -1,6 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -10,6 +13,16 @@ public class CalendarHandling {
         WebDriverManager.firefoxdriver().clearDriverCache().setup();
         WebDriver driver = new FirefoxDriver();
         driver.get("http://seleniumpractise.blogspot.com/search?updated-max=2016-08-09T11:22:00-07:00&max-results=7&start=14&by-date=false");
+
+        driver.findElement(By.id("datepicker")).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("ui-datepicker-calendar")));
+
+
+       String monthYearVal =  driver.findElement(By.className("ui-datepicker-title")).getText();
+
+        System.out.println(monthYearVal);
 
 
     }
